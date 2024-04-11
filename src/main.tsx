3b910e47308786a696a1bb2f74069590ae804dce
UjjwalAglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2024-04-11 16:08:34 +0530

    news

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://github.com/UjjwalAglawe/TON-Counter_2/ton-connect-manifest.json';
+const manifestUrl = 'https://github.com/UjjwalAglawe/TON-Counter_2/blob/main/public/ton-connect-manifestfile.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
